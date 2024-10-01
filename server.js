36ccc910d668e6d4a7c0c9429fd91db2ed24f7b5
Azam Alamov <alamovazamjon@gmail.com>
2024-10-01 20:31:11 +0300

    Uploaded Lab Uno

--- server.js
+++ server.js
@@ -1,28 +1,67 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const multer = require('multer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 7777;
+
+app.use('/uploads', express.static('uploads'));
+app.use('/css', express.static('css'));
+
+
+function generateUniqueId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < 6; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-let notes = [];
+let tasks = [];
 
 app.get('/', (req, res) => {
-res.render('index', { notes });
+    const filterStatus = req.query.filterStatus;
+    let filteredTasks = tasks;
+
+    if (filterStatus) {
+        filteredTasks = tasks.filter(task => task.status === filterStatus);
+    }
+
+    res.render('index', { tasks: filteredTasks, filterStatus });
 });
 
-app.post('/new-note', (req, res) => {
-const { title, content } = req.body;
-notes.push({ title, content });
-res.redirect('/');
+app.post('/add-task', upload.single('file'), (req, res) => {
+    const { title, status, dueDate } = req.body;
+    const file = req.file ? req.file.filename : null;
+    const id = generateUniqueId();
+    tasks.push({ id, title, status, dueDate, file });
+    res.redirect('/');
+});
+
+app.post('/delete-task', (req, res) => {
+    const { id } = req.body;
+    tasks = tasks.filter(task => task.id !== id);
+    res.redirect('/');
 });
 
 app.listen(PORT, () => {
-console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Free Clash of Clans gems => http://localhost:${PORT}`);
 });
